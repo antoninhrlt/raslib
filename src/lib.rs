2eb6e7c502b535dca610ef66cf9095ef821c1ab1
antoninhrlt <antonherault@gmail.com>
2022-03-02 16:23:23 +0100

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,23 @@
+// This file is part of "raslib"
+// Under the MIT License
+// Copyright (c) Antonin Hérault
+
+pub mod gpio;
+pub mod l298n;
+
+pub const HIGH: bool = true;
+pub const LOW: bool = false;
+
+pub fn sleep(milliseconds: u64) {
+    use std::thread;
+    use std::time::Duration;
+    thread::sleep(Duration::from_millis(milliseconds));
+}
+
 #[cfg(test)]
-mod tests {
+pub mod tests {
     #[test]
-    fn it_works() {
+    pub fn it_works() {
         let result = 2 + 2;
         assert_eq!(result, 4);
     }
